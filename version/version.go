941b3d7f83c490391c49969cfa71ba84d8188eb5
David Vennik <stalker.loki@protonmail.ch>
2021-05-17 07:41:09 +0300

    updating

--- version/version.go
+++ version/version.go
@@ -12,12 +12,12 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname
 	GitRef = "refs/heads/main"
 	// GitCommit is the commit hash of the current HEAD
-	GitCommit = "38329e63d273520d89ab7b3ba7a3a2ad6d22d77d"
+	GitCommit = "f49831fe55b3e226205c74b5a5c323bcddf04408"
 	// BuildTime stores the time when the current binary was built
-	BuildTime = "2021-05-16T11:27:52+03:00"
+	BuildTime = "2021-05-17T07:40:21+03:00"
 	// Tag lists the Tag on the build, adding a + to the newest Tag if the commit is
 	// not that commit
-	Tag = "v0.0.13"
+	Tag = "v0.0.14"
 	// PathBase is the path base returned from runtime caller
 	PathBase = "/home/loki/src/github.com/cybriq/qu/"
 	// Major is the major number from the tag
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag
 	Minor = 0
 	// Patch is the patch version number from the tag
-	Patch = 13
+	Patch = 14
 	// Meta is the extra arbitrary string field from Semver spec
 	Meta = ""
 )
